f6c07965ef1be23a5aab69f318002f1e9085becd
Guidosalimbeni <guido.salimbeni@gmail.com>
2020-07-08 20:14:05 +0100

    ading text

--- calculator_project/lib/main.dart
+++ calculator_project/lib/main.dart
@@ -1,40 +1,24 @@
 import 'package:flutter/material.dart';
 
-import './transaction.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'AVACA',
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly Groceries',
-      amount: 16.53,
-      date: DateTime.now(),
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My calculator'),
+        title: Text('LOGO'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -43,60 +27,26 @@ class MyHomePage extends StatelessWidget {
           Container(
             width: double.infinity,
             child: Card(
-              color: Colors.blue,
-              child: Text('CHART!'),
+              color: Theme.of(context).primaryColor,
+              child: Text('PICTURE FIELD!'),
               elevation: 5,
             ),
           ),
-          Column(
-            children: transactions.map((tx) {
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        tx.amount.toString(),
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          tx.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          tx.date.toString(),
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Theme.of(context).primaryColorDark,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  "TAKE A PICTURE OF YOUR STILL LIFE COMPOSITION AND PRESS NEXT",
+                  style: TextStyle(
+                      color: Theme.of(context).primaryColorLight, fontSize: 28),
                 ),
-              );
-            }).toList(),
-          ),
+              ),
+              elevation: 5,
+            ),
+          )
         ],
       ),
     );
